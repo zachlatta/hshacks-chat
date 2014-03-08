cd32ef0fd1632ccd2cf426d8a6877ce2422c5cf3
Zach Latta <zach@zachlatta.com>
2014-03-08 10:20:58 -0800

    Add default profile picture.

--- app/scripts/controllers/chat.js
+++ app/scripts/controllers/chat.js
@@ -3,9 +3,15 @@
 angular.module('hshacksChatApp')
   .controller('ChatCtrl', function ($scope, Chat) {
     $scope.user = 'Guest ';
+    $scope.profilePicture = 'http://i.imgur.com/Vn018nS.jpg';
     $scope.messages = Chat;
     $scope.addMessage = function () {
-      $scope.messages.$add({user: $scope.user, message: $scope.message});
+      $scope.messages.$add({
+        user: $scope.user,
+        message: $scope.message,
+        image: $scope.profilePicture
+      });
+
       $scope.message = '';
     };
   });
