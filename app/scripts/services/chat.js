77e1de386bac3cbaa293ebe24a34ac5624e90819
Zach Latta <zach@zachlatta.com>
2014-03-08 10:13:56 -0800

    Limit chat view to latest 100 images.

--- app/scripts/services/chat.js
+++ app/scripts/services/chat.js
@@ -5,5 +5,5 @@ var Firebase;
 angular.module('hshacksChatApp')
   .factory('Chat', function ($firebase) {
     var ref = new Firebase('https://hshacks.firebaseio.com');
-    return $firebase(ref);
+    return $firebase(ref.limit(100));
   });
